4b2225115911798de3d759b90ab0a091266235a9
Pharadorn Boonruam <61776315+bom2542@users.noreply.github.com>
2021-07-29 12:03:47 +0700

    Update 29/07/2564 12.03

--- src/app/modules/login/login.component.ts
+++ src/app/modules/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
@@ -6,10 +7,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
-
-  constructor() { }
+  form : FormGroup;
+  constructor(private formBuilder: FormBuilder) {}
 
   ngOnInit(): void {
+    this.setFormGroup();
   }
 
+  setFormGroup() {
+    this.form = this.formBuilder.group({
+      username: [null, Validators.required],
+      password: [null, Validators.required],
+    });
+  }
+
+  onSubmit() {
+    if(this.form.invalid){
+      return;
+    }else{
+      console.log("username", this.username.value)
+      console.log("password", this.password.value)
+    }
+  }
+
+  get username(): FormControl {
+    return this.form.get("username") as FormControl;
+  }
+
+  get password(): FormControl {
+    return this.form.get("password") as FormControl;
+  }
 }
--- src/app/modules/login/login.module.ts
+++ src/app/modules/login/login.module.ts
@@ -3,6 +3,9 @@ import { CommonModule } from '@angular/common';
 
 import { LoginRoutingModule } from './login-routing.module';
 import { LoginComponent } from './login.component';
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {SharedModule} from "../../shared/shared.module";
 
 
 @NgModule({
@@ -11,7 +14,10 @@ import { LoginComponent } from './login.component';
   ],
   imports: [
     CommonModule,
-    LoginRoutingModule
+    LoginRoutingModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    SharedModule
   ]
 })
 export class LoginModule { }
