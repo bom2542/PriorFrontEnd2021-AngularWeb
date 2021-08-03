4e9ae044984dd3348c45c3a33e16e4199b853fb7
Pharadorn Boonruam <61776315+bom2542@users.noreply.github.com>
2021-08-03 17:33:19 +0700

    Update 3/08/2564 17.33

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ImageMapCoordinate } from "./modules/image-map/image-map.component";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'prior-angular01';
+  image: string =
+    'https://image.shutterstock.com/image-vector/solar-system-sun-planets-vector-260nw-751091653.jpg';
+
+  coordinates: ImageMapCoordinate[] = [
+    {
+      name: 'The sun',
+      x: 0,
+      y: 200,
+      width: 95,
+      height: 100
+    },
+    {
+      name: 'The sun',
+      x: 0,
+      y: 100,
+      width: 95,
+      height: 100
+    }
+  ];
+
+  showImage: boolean;
+
+  getClick(coordinate: ImageMapCoordinate) {
+    console.log(`Clicked on ${coordinate.name}`);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,11 +7,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {SharedModule} from "./shared/shared.module";
 import { MenuComponent } from './modules/menu/menu.component';
 import {HttpClientModule} from "@angular/common/http";
+import { ImageMapComponent } from './image-map/image-map.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    ImageMapComponent
   ],
   imports: [
     BrowserModule,
