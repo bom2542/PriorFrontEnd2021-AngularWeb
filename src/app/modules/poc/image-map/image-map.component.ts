8ad590b4ef299f6bdbe9f8df354be292acd520ff
Pharadorn Boonruam <61776315+bom2542@users.noreply.github.com>
2021-08-06 10:58:50 +0700

    Update 06/08/2564 10.58

--- src/app/modules/poc/image-map/image-map.component.ts
+++ src/app/modules/poc/image-map/image-map.component.ts
@@ -8,7 +8,28 @@ import { PrimeNGConfig } from 'primeng/api';
   templateUrl: './image-map.component.html',
   styleUrls: ['./image-map.component.scss']
 })
-export class ImageMapComponent implements OnInit, AfterViewInit, OnDestroy {
+export class ImageMapComponent implements OnInit {
+
+  //Value declaration support api from other component and module
+  id : number /* user id */
+  languageMap : number /* 1 = golang, 2 = java and 3 = kotlin */
+  statusTopic : number /* 1 = success, 2 = edit, 3 = ques */
+  ratingTopic  : number /* floating 1.0 - 5.0 and + 0.5 */
+  topicName : string /* topic name of problem */
+  xAxis : number /* x axis same left position to right*/
+  yAxis : number /* y axis same top position to bottom*/
+  ownerId : number /* owner login */
+  gitName : string /* git name from user */
+  gitUrl :string /* git for make branch */
+
+  //Dynamic data (API Json)
+  psg = [
+    {languageMap: 1, statusTopic: 1, ratingTopic: 2.5, topicName: "Honda", xAxis : 50, yAxis : 40, gitName: "bom2542", gitUrl : "PriorFrontEnd2021-AngularWeb"},
+    {languageMap: 1, statusTopic: 1, ratingTopic: 5, topicName: "Honda suzuki", xAxis : 1, yAxis : 88, gitName: "bom2542", gitUrl : "PriorFrontEnd2021-AngularWeb"},
+    {languageMap: 1, statusTopic: 2, ratingTopic: 4, topicName: "Honda suzuki yamaha", xAxis : 61, yAxis : 75, gitName: "bom2542", gitUrl : "PriorFrontEnd2021-AngularWeb"},
+    {languageMap: 1, statusTopic: 3, ratingTopic: 4.5, topicName: "Honda suzuki yamaha bom", xAxis : 20, yAxis : 80, gitName: "bom2542", gitUrl : "PriorFrontEnd2021-AngularWeb"},
+  ];
+
 
   @ViewChild('scene', { static: false }) scene: ElementRef;
   panZoomController;
@@ -49,6 +70,7 @@ export class ImageMapComponent implements OnInit, AfterViewInit, OnDestroy {
 
   showModalDialog() {
     this.displayModal = true;
+    console.log(this.displayModal);
   }
 
   showBasicDialog() {
@@ -125,31 +147,31 @@ export class ImageMapComponent implements OnInit, AfterViewInit, OnDestroy {
     }
   }
 
-  zoomToggle(zoomIn: boolean) {
-    const idx = this.zoomLevels.indexOf(this.currentZoomLevel);
-    if (zoomIn) {
-      if (typeof this.zoomLevels[idx + 1] !== 'undefined') {
-        this.currentZoomLevel = this.zoomLevels[idx + 1];
-      }
-    } else {
-      if (typeof this.zoomLevels[idx - 1] !== 'undefined') {
-        this.currentZoomLevel = this.zoomLevels[idx - 1];
-      }
-    }
-    if (this.currentZoomLevel === 1) {
-      this.panZoomController.moveTo(0, 0);
-      this.panZoomController.zoomAbs(0, 0, 1);
-    } else {
-      this.zoom();
-    }
-  }
+  // zoomToggle(zoomIn: boolean) {
+  //   const idx = this.zoomLevels.indexOf(this.currentZoomLevel);
+  //   if (zoomIn) {
+  //     if (typeof this.zoomLevels[idx + 1] !== 'undefined') {
+  //       this.currentZoomLevel = this.zoomLevels[idx + 1];
+  //     }
+  //   } else {
+  //     if (typeof this.zoomLevels[idx - 1] !== 'undefined') {
+  //       this.currentZoomLevel = this.zoomLevels[idx - 1];
+  //     }
+  //   }
+  //   if (this.currentZoomLevel === 1) {
+  //     this.panZoomController.moveTo(0, 0);
+  //     this.panZoomController.zoomAbs(0, 0, 1);
+  //   } else {
+  //     this.zoom();
+  //   }
+  // }
 
-  ngAfterViewInit() {
-    this.zoomLevels = [0.1, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.5, 3];
-    this.currentZoomLevel = this.zoomLevels[4];
-    // Panzoom(document.querySelector('#scene'));
-    this.panZoomController = Panzoom(this.scene.nativeElement);
-  }
+  // ngAfterViewInit() {
+  //   this.zoomLevels = [0.1, 0.25, 0.5, 0.75, 1, 1.25, 1.5, 1.75, 2, 2.5, 3];
+  //   this.currentZoomLevel = this.zoomLevels[4];
+  //   // Panzoom(document.querySelector('#scene'));
+  //   this.panZoomController = Panzoom(this.scene.nativeElement);
+  // }
 
   ngOnDestroy() {
   }
